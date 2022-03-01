b860156da7efaf0e0a32f6616eb9cb84a4079be5
Olga Mirete <olgamirete@gmail.com>
2022-03-01 20:31:52 +0100

    added signature at the bottom of Footer

--- src/sections/Footer/index.tsx
+++ src/sections/Footer/index.tsx
@@ -1,11 +1,11 @@
-import SocialRow from "../../components/SocialRow"
+import SocialRow from '../../components/SocialRow'
+import { ReactComponent as Signature } from '../../images/svg/signature.svg'
 
 const Footer: React.FC = () => {
   return (
-    <div className="d-flex mt-3 justify-content-center">
-      <h3 className="text-decoration-none me-2 mt-0">
-        <SocialRow className="ms-2" iconClassName="text-muted"/>
-      </h3>
+    <div className="h3 text-decoration-none me-2 mt-3 text-center">
+      <SocialRow className="ms-2" iconClassName="text-muted" />
+      <Signature className="mt-4" style={{ fill: '#6C757D' }} />
     </div>
   )
 }

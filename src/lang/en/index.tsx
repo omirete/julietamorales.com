d9d03ced90bdf5b9bed2ef9735def458ca81952f
Olga Mirete <olgamirete@gmail.com>
2022-03-03 08:33:01 +0100

    bugfix

--- src/lang/en/index.tsx
+++ src/lang/en/index.tsx
@@ -13,7 +13,7 @@ const en = {
   about_2: 'I hold an M.A. in Gender Studies and Communications and a postgraduate degree in Literary Journalism from Universidad Autónoma de Barcelona (Spain).',
   about_3: 'I spent the last 6 years working in the communications field: first as journalist covering feminist issues and then as a creative digital officer.',
   about_4: 'I have worked at international environments such as UNESCO, the Organization of American States (OAS) and WIEGO (an NGO focused on improving conditions for women workers in the informal economy).',
-  about_5: <span>I’m currently working at Equality Now (<a href="https://www.equalitynow.org/staff_members/julieta-morales/" className="link-dark" target="_blank">learn more about my role here!</a>). We use the power of the law to create enduring equality for women and girls everywhere.</span>,
+  about_5: <span>I’m currently working at Equality Now (<a href="https://www.equalitynow.org/staff_members/julieta-morales/" className="link-dark" target="_blank" rel="noreferrer">learn more about my role here!</a>). We use the power of the law to create enduring equality for women and girls everywhere.</span>,
   about_6: 'I’ve lived in the United States, France and Spain.',
 }
 
